e9b0d1256b34e78e7843d8720fb8673dcb3252bd
oguzhankop <oguzhankop@hotmail.com>
2017-07-03 03:18:16 +0300

    First File

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(new Vector3(60, 45, 40) * Time.deltaTime);
+		
+	}
+}
